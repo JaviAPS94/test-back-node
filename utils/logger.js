c26e47d6bee25eb24111af0768cb032cb8296600
alex.pinaida <alex.pinaida@rappi.com>
2025-04-07 16:37:48 -0500

    delete logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,27 +1,24 @@
-import winston from 'winston';
+import winston from "winston";
 
 //2025-10-05T12:00:00.000Z [ERROR]: Error al obtener los productos
 const logFormat = winston.format.printf(({ level, message, timestamp }) => {
-    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-})
+  return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
 
 const customLevels = {
-    error: 0,
-    warning: 1,
-    info: 2,
-    debug: 3,
-}
+  error: 0,
+  warning: 1,
+  info: 2,
+  debug: 3,
+};
 
 const logger = winston.createLogger({
-    levels: customLevels,
-    format: winston.format.combine(
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        logFormat
-    ),
-    transports: [
-        new winston.transports.Console({ level: "debug" }),
-        new winston.transports.File({ filename: "logs/error.log", level: "warning"})
-    ]
-})
+  levels: customLevels,
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    logFormat
+  ),
+  transports: [new winston.transports.Console({ level: "debug" })],
+});
 
 export default logger;
